80a61955cc72b50db6ae658fa200a35928432907
Andrew Deinega <andrew.deinega@backstopdevelopment.com>
2026-02-12 23:33:36 +0200

    add server

--- app.js
+++ app.js
@@ -92,6 +92,9 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-export default app;
+const port = process.env.PORT || 3001;
 
+const server = app.listen(port, () => console.log(`App listening on port ${port}!`));
 
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
